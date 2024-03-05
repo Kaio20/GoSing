cc81693dc860511aa69cedbbe7df6b41835bdef0
Kaio20 <kaiwerder39@gmail.com>
2024-03-05 16:37:31 +0100

    wierd microphone delay seems fixed
    Hopefully it doesnt come back

--- live_audio.gd
+++ live_audio.gd
@@ -9,10 +9,10 @@ func _ready() -> void:
 	record_bus_index = AudioServer.get_bus_index("Record")
 	
 func _process(delta: float) -> void:
-	var sample = remap(AudioServer.get_bus_peak_volume_left_db(record_bus_index,0),-75,10,0,100) 
+	var sample = AudioServer.get_bus_peak_volume_left_db(record_bus_index,0) #,-75,10,0,100) 
 	#buggy da er ohne remap erst deutlich später anfängt
 	
-	var linear_sample = sample #db_to_linear(sample)
+	var linear_sample = db_to_linear(sample)
 	samples.push_front(linear_sample)
 	
 	if samples.size() > 20:
